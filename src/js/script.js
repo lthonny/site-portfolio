e37522da6db9ab0ba3d5c3db6d9632e4a65fd195
Василий <58366884+lthonny@users.noreply.github.com>
2021-06-02 10:28:39 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,10 @@
-// footer
-const block = document.querySelector('.footer-content__block');
-const icons = document.querySelectorAll('.footer-content__block-icons');
-
-console.log(icons);
-
-block.addEventListener('mouseover', event => {
-	console.log('Mouse in');
-	icons.style.backgroundColor = '#fff';
-});
-// block.addEventListener('click', () => alert('1'));
+const numb = document.querySelector('.numb');
+let counter = 0;
+setInterval(() => {
+	if (counter == 30) {
+		clearInterval();
+	} else {
+		counter += 1;
+		numb.textContent = counter + '%';
+	}
+}, 80);
