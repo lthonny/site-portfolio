b462ecae2c4078cf6e9f4575e759ed4e60d0bafb
lthonny <thonny02@mail.ru>
2021-06-09 19:33:36 +0300

    almost ready for release

--- src/js/script.js
+++ src/js/script.js
@@ -21,3 +21,15 @@ $('a[href*="#"]').on('click', function (e) {
 		'linear'
 	);
 });
+
+$(function () {
+	$('.skill_per').each(function () {
+		$this = $(this);
+		var per = $(this).attr('per');
+		$this.css('width', per + '%');
+		$this
+			.find('.value')
+			.text(per + '%')
+			.css('opacity', '1');
+	});
+});
